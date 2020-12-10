5cb7f9fedbf3c5b3da45b06bfb2d378fac9d7ead
Jonathan Solorzano-Hamilton <jsolorzanohamilton@netflix.com>
2020-12-09 17:53:26 -0800

    Day 9 initial commit

---
+++ app/src/main/kotlin/adventofcode2020/Day9.kt
@@ -0,0 +1,15 @@
+package adventofcode2020
+
+class Day9(resource: Resource) : ResourceSolver(resource) {
+    override fun solve1(): Long {
+        TODO("Implement")
+    }
+    override fun solve2(): Long {
+        TODO("Implement")
+    }
+}
+
+class XmasCipher(encode: List<String) {
+
+}
+
