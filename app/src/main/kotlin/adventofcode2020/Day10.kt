7527171d847b9a2933900b8064f6175039a59e86
Jonathan Solorzano-Hamilton <jsolorzanohamilton@netflix.com>
2020-12-10 10:54:34 -0800

    Stub pseudocode for 10.1

---
+++ app/src/main/kotlin/adventofcode2020/Day10.kt
@@ -0,0 +1,10 @@
+// Day 10, fun with linked lists
+
+// build a linked list (probably sort the set first)
+
+// next() should exception if diff > 3
+
+// peek at next to emit the delta
+
+// iterate over the list and collect the deltas into counters, probably hashmap for extensibility later
+
